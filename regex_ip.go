48d9ae9750031d3888e77b20eeb1702c2e13560d
Heiko Reese <mail@heiko-reese.de>
2017-01-08 22:09:04 +0100

    More tests

--- regex_ip.go
+++ regex_ip.go
@@ -7,17 +7,6 @@ import (
 const (
 	// this will not match the unspecified address "::" (RfC 4291, Section 2.5.2.)
 	regexMatchV6 = `(
-        # addresses containing a single ::
-                                                      :: (?:(?:[[:alnum:]]{1,4}:){0,6}(?:[[:alnum:]]{1,4})){0,1} | 
-                                 (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,5}(?:[[:alnum:]]{1,4})){0,1} |
-        (?:[[:alnum:]]{1,4}:){1} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,4}(?:[[:alnum:]]{1,4})){0,1} |
-        (?:[[:alnum:]]{1,4}:){2} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,3}(?:[[:alnum:]]{1,4})){0,1} |
-        (?:[[:alnum:]]{1,4}:){3} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,2}(?:[[:alnum:]]{1,4})){0,1} |
-        (?:[[:alnum:]]{1,4}:){4} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,1}(?:[[:alnum:]]{1,4})){0,1} |
-        (?:[[:alnum:]]{1,4}:){5} (?:[[:alnum:]]{1,4}) :: (?:[[:alnum:]]{1,4}){0,1}                               |
-        (?:[[:alnum:]]{1,4}:){6} (?:[[:alnum:]]{1,4}) ::                                                         |
-        # plain IPv6 address
-        (?:[[:alnum:]]{1,4}:){7} (?:[[:alnum:]]{1,4}) |
         # IPv4-embedded IPv6 addresses
         (?:64:ff9b::                                                                                  # Well-Known Prefix
             (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) # embedded IPv4 address
@@ -28,7 +17,18 @@ const (
         # IPv4-Mapped IPv6 Address
         (?: :: [fF]{4}:                                                                               # ::ffff:
             (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) # embedded IPv4 address
-        )
+        ) |
+        # addresses containing a single ::
+                                                      :: (?:(?:[[:alnum:]]{1,4}:){0,6}(?:[[:alnum:]]{1,4})){0,1} |
+                                 (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,5}(?:[[:alnum:]]{1,4})){0,1} |
+        (?:[[:alnum:]]{1,4}:){1} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,4}(?:[[:alnum:]]{1,4})){0,1} |
+        (?:[[:alnum:]]{1,4}:){2} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,3}(?:[[:alnum:]]{1,4})){0,1} |
+        (?:[[:alnum:]]{1,4}:){3} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,2}(?:[[:alnum:]]{1,4})){0,1} |
+        (?:[[:alnum:]]{1,4}:){4} (?:[[:alnum:]]{1,4}) :: (?:(?:[[:alnum:]]{1,4}:){0,1}(?:[[:alnum:]]{1,4})){0,1} |
+        (?:[[:alnum:]]{1,4}:){5} (?:[[:alnum:]]{1,4}) :: (?:[[:alnum:]]{1,4}){0,1}                               |
+        (?:[[:alnum:]]{1,4}:){6} (?:[[:alnum:]]{1,4}) ::                                                         |
+        # plain IPv6 address
+        (?:[[:alnum:]]{1,4}:){7} (?:[[:alnum:]]{1,4})
         )`
 	regexMatchV4 = `
         (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}
@@ -39,9 +39,8 @@ var (
 	regexRemoveComments = regexp.MustCompile("(?m:#.*$)")
 	regexRemoveSpaces   = regexp.MustCompile("(?m:[[:space:]]+)")
 	MatchIPv4           = MustCompileReadableRegex(regexMatchV4)
+	MatchBoundedIPv4    = MustCompileReadableRegex(`(?:\b` + regexMatchV4 + `\b)`)
 	MatchIPv6           = MustCompileReadableRegex(regexMatchV6)
-	MatchBoundedIPv4    = MustCompileReadableRegex(`\b` + regexMatchV4 + `\b`)
-	MatchBoundedIPv6    = MustCompileReadableRegex(`\b` + regexMatchV6 + `\b`)
 )
 
 // MustCompileReadableRegex is similar to regexp.MustCompile
--- regex_ip_test.go
+++ regex_ip_test.go
@@ -14,25 +14,49 @@ type IPFindAllString struct {
 
 var IPFindAllStringTests = map[string][]IPFindAllString{
 	"MatchIPv4": []IPFindAllString{
-		IPFindAllString{*MatchIPv4, "1.1.1.1", []string{"1.1.1.1"}},
-		IPFindAllString{*MatchIPv4, "127.0.0.1", []string{"127.0.0.1"}},
-		IPFindAllString{*MatchIPv4, "8.9.10.333", []string{"8.9.10.33"}},
-		IPFindAllString{*MatchIPv4, `"8.8.8.8"`, []string{"8.8.8.8"}},
-		IPFindAllString{*MatchIPv4, `999.0.0.0`, []string{"99.0.0.0"}},
+		IPFindAllString{*MatchIPv4, `1.1.1.1`, []string{`1.1.1.1`}},
+		IPFindAllString{*MatchIPv4, `127.0.0.1`, []string{`127.0.0.1`}},
+		IPFindAllString{*MatchIPv4, `8.9.10.333`, []string{`8.9.10.33`}},
+		IPFindAllString{*MatchIPv4, `"8.8.8.8"`, []string{`8.8.8.8`}},
+		IPFindAllString{*MatchIPv4, `999.0.0.888`, []string{`99.0.0.88`}},
+		IPFindAllString{*MatchIPv4, `1.2.3.4 5.6.7.8 9.10.200.1`, []string{`1.2.3.4`, `5.6.7.8`, `9.10.200.1`}},
 	},
 	"MatchBoundedIPv4": []IPFindAllString{
-		IPFindAllString{*MatchBoundedIPv4, "_1.1.1.1_", []string{"1.1.1.1"}},
-		IPFindAllString{*MatchBoundedIPv4, "1127.0.0.1", []string{"127.0.0.1"}},
-		IPFindAllString{*MatchBoundedIPv4, "8.9.10.333", []string{"8.9.10.33"}},
-		IPFindAllString{*MatchBoundedIPv4, `"8.8.8.8"`, []string{"8.8.8.8"}},
-		IPFindAllString{*MatchBoundedIPv4, `xxx9.1.0.0xxx`, []string{"9.1.0.0"}},
+		IPFindAllString{*MatchBoundedIPv4, `1.1.1.1`, []string{`1.1.1.1`}},
+		IPFindAllString{*MatchBoundedIPv4, `127.0.0.1`, []string{`127.0.0.1`}},
+		IPFindAllString{*MatchBoundedIPv4, `8.9.10.333`, []string(nil)},
+		IPFindAllString{*MatchBoundedIPv4, `"8.8.8.8"`, []string{`8.8.8.8`}},
+		IPFindAllString{*MatchBoundedIPv4, `999.0.0.888`, []string(nil)},
+		IPFindAllString{*MatchBoundedIPv4, `1.2.3.4 5.6.7.8 9.10.200.1`, []string{`1.2.3.4`, `5.6.7.8`, `9.10.200.1`}},
+		IPFindAllString{*MatchBoundedIPv4, `::1.2.3.4:5.6.7.8:9.10.200.1:::`, []string{`1.2.3.4`, `5.6.7.8`, `9.10.200.1`}},
+	},
+	"MatchIPv6": []IPFindAllString{
+		IPFindAllString{*MatchIPv6, `::0`, []string{`::0`}},
+		IPFindAllString{*MatchIPv6, `::1`, []string{`::1`}},
+		IPFindAllString{*MatchIPv6, `::ff`, []string{`::ff`}},
+		IPFindAllString{*MatchIPv6, `1::`, []string{`1::`}},
+		IPFindAllString{*MatchIPv6, `111111111::aaaaaaaa`, []string{`1111::aaaa`}},
+		IPFindAllString{*MatchIPv6, `a::b`, []string{`a::b`}},
+		IPFindAllString{*MatchIPv6, `1234:5678::90ab`, []string{`1234:5678::90ab`}},
+		IPFindAllString{*MatchIPv6, `2001:db8::1`, []string{`2001:db8::1`}},
+		IPFindAllString{*MatchIPv6, `1:2:3:4:5:6:7:8`, []string{`1:2:3:4:5:6:7:8`}},
+		IPFindAllString{*MatchIPv6, `12:34:56:78:90:ab:cd:ef`, []string{`12:34:56:78:90:ab:cd:ef`}},
+		IPFindAllString{*MatchIPv6, `12::56:78:90:ab:cd:ef`, []string{`12::56:78:90:ab:cd:ef`}},
+		IPFindAllString{*MatchIPv6, `12:34::78:90:ab:cd:ef`, []string{`12:34::78:90:ab:cd:ef`}},
+		IPFindAllString{*MatchIPv6, `12:34:56::90:ab:cd:ef`, []string{`12:34:56::90:ab:cd:ef`}},
+		IPFindAllString{*MatchIPv6, `12:34:56:78::ab:cd:ef`, []string{`12:34:56:78::ab:cd:ef`}},
+		IPFindAllString{*MatchIPv6, `12:34:56:78:90::cd:ef`, []string{`12:34:56:78:90::cd:ef`}},
+		IPFindAllString{*MatchIPv6, `12:34:56:78:90:ab::ef`, []string{`12:34:56:78:90:ab::ef`}},
+		IPFindAllString{*MatchIPv6, `::ffff:255.255.255.255`, []string{`::ffff:255.255.255.255`}},
+		IPFindAllString{*MatchIPv6, `2001:db8:3:4::192.0.2.33`, []string{`2001:db8:3:4::192.0.2.33`}},
+		IPFindAllString{*MatchIPv6, `64:ff9b::129.13.64.5`, []string{`64:ff9b::129.13.64.5`}},
 	},
 }
 
-func TestFindIPv4(t *testing.T) {
+func TestIPFindAllString(t *testing.T) {
 	for comment, tests := range IPFindAllStringTests {
 		for _, test := range tests {
-			res := MatchIPv4.FindAllString(test.input, -1)
+			res := test.re.FindAllString(test.input, -1)
 			if !reflect.DeepEqual(res, test.result) {
 				t.Errorf("Testset %s tested\n  %s\n and expected\n  %#v\ngot\n  %#v\n", comment, test.input, test.result, res)
 			}
